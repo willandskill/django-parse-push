018d062f2ed9ca9acd3d555d439dd81b89c88a6f
Faisal Mahmud <faisal@willandskill.se>
2016-01-05 17:28:40 +0100

    Use .get_latest() instead so that we do not override built-in .latest()

--- parse_push/managers.py
+++ parse_push/managers.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class DeviceManager(models.Manager):
 
-    def latest(self):
+    def get_latest(self):
         """ Returns latest Device instance """
-        return self.latest('created_at')
+        return self.get_queryset().latest('created_at')
 
