073f1fb587e92c9d81f98978cbcf33eff86f42bf
Faisal Mahmud <faisal@willandskill.se>
2016-01-05 11:09:18 +0100

    Add validation to DeviceSerializer

--- parse_push/serializers.py
+++ parse_push/serializers.py
@@ -4,10 +4,25 @@ from .enums import DeviceKind
 from .models import Device
 
 
-class DeviceSerializer(serializers.ModelSerializer):
+class DeviceSerializer(serializers.Serializer):
     token = serializers.CharField(min_length=20, max_length=100)
     kind = serializers.ChoiceField(DeviceKind.choicify())
 
-    class Meta:
-        model = Device
-        fields = ('token', 'kind')
+    def create(self, validated_data):
+        # TODO: Wrap this logic in a serializer field.
+        choices = DeviceKind.choicify()
+        dict_ = {k.upper(): v for v, k in choices}
+        data = {
+            'profile': validated_data['profile'],
+            'token': validated_data['token'],
+            'kind': dict_.get(validated_data['kind'])
+        }
+        return Device.objects.create(**data)
+
+    def validate_kind(self, value):
+        """ Check that kind is valid. """
+        keys = DeviceKind.get_keys()
+        if value not in keys:
+            raise serializers.ValidationError("Invalid kind ({}) passed! Must be one of {}.".format(value, ', '.join(keys)))
+        return value
+
