7f9ad5c289a5f888dd949ed5247918e71837a25e
Faisal Mahmud <faisal@willandskill.se>
2016-01-15 18:11:01 +0100

    Bumped up to v. 0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-parse-push',
-    version='0.1.2',
+    version='0.1.3',
     author=u'Will & Skill AB',
     author_email='info@willandskill.se',
     packages=find_packages(),
